46011405f9049a1363102ada2acbc7200076fbe5
Jeonhaeun <21900646@handong.edu>
2025-03-07 22:56:09 +0900

    refactor: 메서드 추상화

--- src/main/java/cleancode/mission/order/service/OrderService.java
+++ src/main/java/cleancode/mission/order/service/OrderService.java
@@ -14,13 +14,13 @@ public class OrderService {
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
     public boolean validateOrder(Order order) {
-        if (order.getItems().size() == 0) {
+        if (hasNoOrderItems(order)) {
             log.info("주문 항목이 없습니다.");
             return false;
         }
 
-        if (order.getTotalPrice() > 0) {
-            if (!order.hasCustomerInfo()) {
+        if (hasValidTotalPrice(order)) {
+            if (hasNoCustomerInfo(order)) {
                 log.info("사용자 정보가 없습니다.");
                 return false;
             }
@@ -29,5 +29,17 @@ public class OrderService {
 
         log.info("올바르지 않은 총 가격입니다.");
         return false;
+    }
+
+    private static boolean hasNoOrderItems(Order order) {
+        return order.getItems().size() == 0;
+    }
+
+    private static boolean hasValidTotalPrice(Order order) {
+        return order.getTotalPrice() > 0;
+    }
+
+    private static boolean hasNoCustomerInfo(Order order) {
+        return !order.hasCustomerInfo();
     }
 }
