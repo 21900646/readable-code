288f1f71ec9fb4b207e0f948275b987312fceab8
Jeonhaeun <21900646@handong.edu>
2025-03-07 22:38:04 +0900

    refactor: 공백 라인으로 의미 부여하기

--- src/main/java/cleancode/mission/order/service/OrderService.java
+++ src/main/java/cleancode/mission/order/service/OrderService.java
@@ -18,6 +18,7 @@ public class OrderService {
             log.info("주문 항목이 없습니다.");
             return false;
         }
+
         if (order.getTotalPrice() > 0) {
             if (!order.hasCustomerInfo()) {
                 log.info("사용자 정보가 없습니다.");
@@ -25,6 +26,7 @@ public class OrderService {
             }
             return true;
         }
+
         log.info("올바르지 않은 총 가격입니다.");
         return false;
     }
